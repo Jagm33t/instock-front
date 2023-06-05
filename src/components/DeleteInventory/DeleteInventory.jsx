32b3705b4b40a6a3fd3ec85f561378b48ff06e1c
xieyutia <sunnyyutian@outlook.com>
2023-06-05 16:48:48 -0700

    add basic div

---
+++ src/components/DeleteInventory/DeleteInventory.jsx
@@ -0,0 +1,21 @@
+function DeleteInventory() {
+  return (
+    <div>
+      <div>
+        <h1>Delete Television inventory item?</h1>
+      </div>
+      <div>
+        Please confirm that you’d like to delete Television from the inventory
+        list. You won’t be able to undo this action.
+      </div>
+      <div>
+        <button>
+          <p>Cancel</p>
+        </button>
+        <button>
+          <p>Delete</p>
+        </button>
+      </div>
+    </div>
+  );
+}
