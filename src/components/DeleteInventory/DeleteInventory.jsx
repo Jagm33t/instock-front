afb633e0c02c4542c015a92383c0ae8f3a269859
xieyutia <sunnyyutian@outlook.com>
2023-06-06 13:54:33 -0700

    add close img

--- src/components/DeleteInventory/DeleteInventory.jsx
+++ src/components/DeleteInventory/DeleteInventory.jsx
@@ -2,6 +2,10 @@ function DeleteInventory() {
   return (
     <div>
       <div>
+        {" "}
+        <img src="../../assets/Icons/close-24px.svg" alt="" /> X{" "}
+      </div>
+      <div>
         <h1>Delete Television inventory item?</h1>
       </div>
       <div>
