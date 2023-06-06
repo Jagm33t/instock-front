2cd5eae458f5365e95f156fd41e687972fbd2cd3
Isaba Nusrat <isabansrt@gmail.com>
2023-06-06 06:10:14 -0700

    Created routes for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import WareHousesPage from "./pages/WareHousesPage/WareHousesPage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WareHousesPage />} />
+          <Route path="/warehouses" element={<Navigate to="/" />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/WareHousesPage/WareHousesPage.jsx
@@ -0,0 +1,8 @@
+function WareHousesPage() {
+  return (
+    <div>
+      <h1>WareHouses</h1>
+    </div>
+  );
+}
+export default WareHousesPage;
