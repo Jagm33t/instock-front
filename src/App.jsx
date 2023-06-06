df9f27927190d96d817b9cad4a9ef14a6c484e5a
Isaba Nusrat <isabansrt@gmail.com>
2023-06-06 14:10:48 -0700

    add form routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import WareHousesPage from "./pages/WareHousesPage/WareHousesPage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import EditWareHousesForm from "./pages/EditWareHousesForm/EditWareHousesForm";
+import AddNewWareHousesForm from "./pages/AddNewWareHousesForm/AddNewWareHousesForm";
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
           <Route path="/" element={<WareHousesPage />} />
           <Route path="/warehouses" element={<Navigate to="/" />} />
           <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/warehouses/edit" element={<EditWareHousesForm />} />
+          <Route path="/warehouses/add" element={<AddNewWareHousesForm />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
