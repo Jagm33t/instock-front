fdf1bee9f61443dce54ea9516379da5f5e96181d
Karine Kotsubo <kkotsubo@proton.me>
2023-06-07 14:09:04 -0700

    mobile style

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -9,78 +9,82 @@ import "./InventoryList.scss";
 function InventoryList() {
   return (
     <section className="card">
-      <div className="card__bgBlue">
-        <div className="card__wrapper">
-          <div className="card__header">
-            <h1 className="card__header-title">Inventory</h1>
-            <div className="card__container">
-              <div className="card__searchBox">
-                <input
-                  type="search"
-                  className="card__searchBox-input"
-                  id="searchBox"
-                  placeholder="Search..."
-                />
-                <img
-                  src={searchImg}
-                  alt={searchImg}
-                  className="card__searchBox-img"
-                />
-              </div>
-              <div className="btn">
-                <div className="btn__style-link">
-                  <button type="button" className="btn__style">
-                    + Add New Item
-                  </button>
-                </div>
+      <div className="card__bgBlue"></div>
+
+      <div className="card__wrapper">
+        <div className="card__header">
+          <h1 className="card__header-title">Inventory</h1>
+          <div className="card__container">
+            <div className="card__searchBox">
+              <input
+                type="search"
+                className="card__searchBox-input"
+                id="searchBox"
+                placeholder="Search..."
+              />
+              <img
+                src={searchImg}
+                alt={searchImg}
+                className="card__searchBox-img"
+              />
+            </div>
+            <div className="btn">
+              <div className="btn__style-link">
+                <button type="button" className="btn__style">
+                  + Add New Item
+                </button>
               </div>
             </div>
           </div>
-          <ul>
-            {/* {inventoryList.map((inventory) => ( ))} */}
-            <li className="card__list">
-              <div className="card__list-content">
-                <div className="card__list-content-left">
-                  <div className="card__list-wrap">
-                    <h4 className="card__list-title ">Inventory Item</h4>
-                    <Link to="/" className="card__product-item">
-                      <p className="card__list-text-item card__list-text-item--product">
-                        Television
-                      </p>
-                      <img
-                        src={chevronRight}
-                        alt={chevronRight}
-                        className="card__product-item_chevron"
-                      />
-                    </Link>
-                  </div>
-                  <div className="card__list-wrap">
-                    <h4 className="card__list-title">Category</h4>
-                    <p className="card__list-text-item">Eletronics</p>
-                  </div>
-                </div>
-                <div className="card__list-content-right">
-                  <div className="card__list-wrap">
-                    <h4 className="card__list-title">Status</h4>
-                    <p className="card__list-text-item">In stock</p>
-                  </div>
-                  <div className="card__list-wrap">
-                    <h4 className="card__list-title">Qty</h4>
-                    <p className="card__list-text-item">500</p>
-                  </div>
-                  <div className="card__list-wrap">
-                    <h4 className="card__list-title">Warehouse</h4>
-                    <p className="card__list-text-item">Manhattan</p>
-                  </div>
-                </div>
-              </div>
-              <div className="card__list-actions">
-                <img src={deleteImg} alt={deleteImg} className="deleteImg" />
-                <img src={editImg} alt={editImg} className="editImg" />
-              </div>
-            </li>
-          </ul>
         </div>
+        <ul>
+          {/* {inventoryList.map((inventory) => ( ))} */}
+          <li className="card__list">
+            <div className="card__list-content">
+              <div className="card__list-content-left">
+                <div className="card__list-wrap">
+                  <h4 className=" card__list-title card__list-title-inventory ">
+                    Inventory Item
+                  </h4>
+                  <Link to="/" className="card__product-item">
+                    <p className="card__list-text-item card__list-text-item--product">
+                      Television
+                    </p>
+                    <img
+                      src={chevronRight}
+                      alt={chevronRight}
+                      className="card__product-item_chevron"
+                    />
+                  </Link>
+                </div>
+                <div className="card__list-wrap">
+                  <h4 className="card__list-title">Category</h4>
+                  <p className="card__list-text-item">Eletronics</p>
+                </div>
+              </div>
+              <div className="card__list-content-right">
+                <div className="card__list-wrap">
+                  <h4 className="card__list-title">Status</h4>
+                  <p className="card__list-text-item-inStock card__list-text-item-outStock">
+                    In stock
+                  </p>
+                </div>
+                <div className="card__list-wrap">
+                  <h4 className="card__list-title">Qty</h4>
+                  <p className="card__list-text-item">500</p>
+                </div>
+                <div className="card__list-wrap">
+                  <h4 className="card__list-title">Warehouse</h4>
+                  <p className="card__list-text-item">Manhattan</p>
+                </div>
+              </div>
+            </div>
+            <div className="card__list-actions">
+              <img src={deleteImg} alt={deleteImg} className="deleteImg" />
+              <img src={editImg} alt={editImg} className="editImg" />
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,14 +1,11 @@
 import InventoryList from "../../components/InventoryList/InventoryList";
 
 function InventoryPage() {
-<<<<<<< HEAD
   return (
     <div>
       <InventoryList />
     </div>
   );
-=======
   return <div></div>;
->>>>>>> develop
 }
 export default InventoryPage;
