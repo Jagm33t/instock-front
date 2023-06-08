4aaefd94edcd55e4886421513b20ac734ce06b8d
Karine Kotsubo <kkotsubo@proton.me>
2023-06-07 21:26:15 -0700

    get data and add some style

--- src/pages/EditWareHousesForm/EditWareHousesForm.jsx
+++ src/pages/EditWareHousesForm/EditWareHousesForm.jsx
@@ -1,7 +1,9 @@
+import WarehouseDetailsList from "../../components/WareHouseDetailsList/WarehouseDetailsList";
 function EditWareHousesForm() {
   return (
     <div>
       <h1>Edit Warehouses Form</h1>
+      <WarehouseDetailsList />
     </div>
   );
 }
